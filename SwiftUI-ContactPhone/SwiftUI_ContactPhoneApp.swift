8c6af6f3b487536b818225d02b30dc0549458680
SreytouchLang <atouch.it@gmail.com>
2023-12-10 14:55:29 -0800

    Initial Commit

---
+++ SwiftUI-ContactPhone/SwiftUI_ContactPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ContactPhoneApp.swift
+//  SwiftUI-ContactPhone
+//
+//  Created by Sreytouch(Jessica) on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ContactPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
